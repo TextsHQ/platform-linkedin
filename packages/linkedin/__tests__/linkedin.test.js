53f52ba4f1afa8a3ffe75138f117d477fa34c17f
Emilio Villagran <emilio@villagran.cl>
2021-01-27 15:59:31 -0300

    feat(linkedin): delete linkedin old class module

--- packages/linkedin/__tests__/linkedin.test.js
+++ packages/linkedin/__tests__/linkedin.test.js
@@ -1,16 +1,9 @@
 "use strict";
 
-const LinkedIn = require("../lib/linkedin").default;
+const LinkedIn = require("../lib/index").default;
 jest.setTimeout(30000);
 
 describe("linkedin module", () => {
   // TODO: Create test
-  it("works", async () => {
-    const linkedin = new LinkedIn();
-    // const cookieSession = await linkedin.login();
-
-    const cookieSession = '' // FIXME
-    const threads = await linkedin.getMessages(cookieSession)
-    console.log(`Threads Length: ${threads.length}`);
-  });
+  it("works", async () => {});
 });
