be30a87b61a0b373d7073409b04b315f195c4d92
Emilio Villagran <emilio@villagran.cl>
2021-01-27 16:51:33 -0300

    feat(thread): use constant

--- packages/linkedin/src/lib/messages.page/intercept-thread-response.ts
+++ packages/linkedin/src/lib/messages.page/intercept-thread-response.ts
@@ -1,4 +1,5 @@
 import * as puppeteer from "puppeteer";
+import { LINKEDIN_CONVERSATIONS_ENDPOINT } from "../constants/linkedin";
 import { filterByType } from "./helpers/filter-by-type";
 
 export let thread: any = {};
@@ -6,9 +7,8 @@ export let thread: any = {};
 export const interceptThreadResponse = async (
   response: puppeteer.Response
 ): Promise<void> => {
-  const conversationApiEndpoint = "/api/messaging/conversations";
   const responseUrl = response.url();
-  const shouldIntercept = responseUrl.includes(conversationApiEndpoint);
+  const shouldIntercept = responseUrl.includes(LINKEDIN_CONVERSATIONS_ENDPOINT);
 
   if (shouldIntercept) {
     const res: any = await response.json();
@@ -19,10 +19,7 @@ export const interceptThreadResponse = async (
       "com.linkedin.voyager.identity.shared.MiniProfile"
     );
 
-    let events = filterByType(
-      included,
-      "com.linkedin.voyager.messaging.Event"
-    );
+    let events = filterByType(included, "com.linkedin.voyager.messaging.Event");
 
     const members = filterByType(
       included,
