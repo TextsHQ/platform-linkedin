9b0de649fcd8aaaa268aad4900ec1e5f6019500c
Emilio Villagran <emilio@villagran.cl>
2021-01-27 16:52:14 -0300

    feat(threads): move parse function to helpers

--- packages/linkedin/src/lib/messages.page/intercept-threads-response.ts
+++ packages/linkedin/src/lib/messages.page/intercept-threads-response.ts
@@ -1,55 +1,19 @@
 import * as puppeteer from "puppeteer";
-import { filterByType } from "./helpers/filter-by-type";
+
+import { LINKEDIN_CONVERSATIONS_ENDPOINT } from "../constants/linkedin";
+import { parseConversationResponse } from "./helpers/parse-conversation-response";
 
 export let messagesThreads = [];
 
 export const interceptMessagesThreadsResponse = async (
   response: puppeteer.Response
 ): Promise<void> => {
-  const conversationApiEndpoint = "/api/messaging/conversations";
   const responseUrl = response.url();
-  const shouldIntercept = responseUrl.includes(conversationApiEndpoint);
+  const shouldIntercept = responseUrl.includes(LINKEDIN_CONVERSATIONS_ENDPOINT);
 
   if (shouldIntercept) {
     const res: any = await response.json();
-    const { included = [] } = res;
-
-    const entities = filterByType(
-      included,
-      "com.linkedin.voyager.identity.shared.MiniProfile"
-    );
-
-    const conversations = filterByType(
-      included,
-      "com.linkedin.voyager.messaging.Conversation"
-    );
-
-    const messagingMembers = filterByType(
-      included,
-      "com.linkedin.voyager.messaging.MessagingMember"
-    );
-
-    const parsedData = entities.reduce((prev, current) => {
-      const entityId = current?.entityUrn.split(":").pop();
-
-      const conversation = conversations.find((conversation) => {
-        return conversation["*participants"].some((participant) =>
-          participant.includes(entityId)
-        );
-      });
-
-      const messagingMember = messagingMembers.find((member) => {
-        return member.entityUrn.includes(entityId);
-      });
-
-      const currentData = {
-        entity: current,
-        messagingMember,
-        conversation,
-      };
-
-      return [...prev, currentData];
-    }, []);
+    const parsedData = parseConversationResponse(res);
 
     messagesThreads = [...messagesThreads, ...parsedData];
   }
