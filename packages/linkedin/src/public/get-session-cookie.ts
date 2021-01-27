54419ccc83d6c1bcbdf837dfff5390861179439b
Emilio Villagran <emilio@villagran.cl>
2021-01-27 16:52:48 -0300

    feat(public): headless false on session

--- packages/linkedin/src/public/get-session-cookie.ts
+++ packages/linkedin/src/public/get-session-cookie.ts
@@ -1,7 +1,7 @@
 import { openBrowser, closeBrowser } from "../lib";
 
 export const getSessionCookie = async (): Promise<string> => {
-  const blank = await openBrowser();
+  const blank = await openBrowser(false);
   const login = await blank.currentPage.goTo.Login(blank);
 
   const sessionCookie = await login.currentPage.getSessionCookie(login)
