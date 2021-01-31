d9a70ac382da1d84694d49e4edbdeb9ee6635904
Emilio Villagran <emilio@villagran.cl>
2021-01-30 23:12:06 -0300

    fix: require

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import type { Platform } from '@textshq/platform-sdk'
 
 export default {
   get info() {
+    // eslint-disable-next-line global-require
     return require('./info').default
   },
 
   get api() {
+    // eslint-disable-next-line global-require
     return require('./api').default
   },
 } as Platform
