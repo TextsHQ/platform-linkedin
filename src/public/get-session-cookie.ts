aa154ba5ad087ba3264aa1dd2dd2e3560aa0d00f
Emilio Villagran <emilio@villagran.cl>
2021-01-31 17:32:51 -0300

    feat(login): get session cookie with credentials

--- src/public/get-session-cookie.ts
+++ src/public/get-session-cookie.ts
@@ -1,10 +1,10 @@
 import { openBrowser, closeBrowser } from '../lib'
 
-export const getSessionCookie = async (): Promise<string> => {
-  const blank = await openBrowser(false)
+export const getSessionCookie = async (loginCredentials: { username: string; password: string }): Promise<string> => {
+  const blank = await openBrowser()
   const login = await blank.currentPage.goTo.Login(blank)
 
-  const sessionCookie = await login.currentPage.getSessionCookie(login)
+  const sessionCookie = await login.currentPage.getSessionCookie(login, loginCredentials)
 
   await closeBrowser(login)
 
