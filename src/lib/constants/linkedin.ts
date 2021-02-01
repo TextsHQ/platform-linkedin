5aea3d0904bf04066be34ba2d417c4eff833a1f5
Emilio Villagran <emilio@villagran.cl>
2021-01-31 21:12:11 -0300

    feat(constants): add user metada endpoint

--- src/lib/constants/linkedin.ts
+++ src/lib/constants/linkedin.ts
@@ -3,3 +3,4 @@ export const LOGIN_URL = 'https://www.linkedin.com/login'
 export const THREADS_URL = 'https://www.linkedin.com/messaging'
 
 export const LINKEDIN_CONVERSATIONS_ENDPOINT = '/api/messaging/conversations'
+export const LINKEDIN_USER_ENDPOINT = '/api/metadata/user'
