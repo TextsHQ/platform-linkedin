a76d1a19594a6f81b35888045433cb0659995656
Emilio Villagran <emilio@villagran.cl>
2021-02-06 23:10:44 -0300

    feat(constants): add feed url

--- src/lib/constants/linkedin.ts
+++ src/lib/constants/linkedin.ts
@@ -1,6 +1,7 @@
 export const LINKEDIN_BASE = '.www.linkedin.com'
 export const LOGIN_URL = 'https://www.linkedin.com/login'
 export const THREADS_URL = 'https://www.linkedin.com/messaging'
+export const FEED_URL = 'https://www.linkedin.com/feed'
 
 export const LINKEDIN_CONVERSATIONS_ENDPOINT = '/api/messaging/conversations'
 export const LINKEDIN_USER_ENDPOINT = '/api/metadata/user'
