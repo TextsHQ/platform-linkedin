9acea6a887c14b05ebb279180b9badd5c3634ea1
Emilio Villagran <emilio@villagran.cl>
2021-02-08 19:51:42 -0300

    feat(constants): update supported emojis

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,9 @@
 import { SupportedReaction } from "@textshq/platform-sdk";
 
 export const supportedReactions: Record<string, SupportedReaction> = {
-  heart: { title: 'Heart', render: '❤️' },
+  clap: { title: 'Clap', render: '👏' },
   like: { title: 'Like', render: '👍' },
   dislike: { title: 'Dislike', render: '👎' },
-  laugh: { title: 'Laugh', render: '😂' },
-  surprised: { title: 'Surprised', render: '😲' },
-  cry: { title: 'Cry', render: '😢' },
-  fire: { title: 'Lit', render: '🔥' },
-  angry: { title: 'Angry', render: '😠' },
-  mask: { title: 'Mask', render: '😷' },
+  smiling: { title: 'Smiling', render: '😊' },
+  disappointed: { title: 'Disappointed', render: '😞' },
 }
