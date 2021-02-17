1587f7b04919d8e6c8fa9bada84b1f4fbf666f6f
Emilio Villagran <emilio@villagran.cl>
2021-02-16 22:21:44 -0300

    feat(constants): add realtime endpoint

--- src/constants/linkedin.ts
+++ src/constants/linkedin.ts
@@ -1,5 +1,7 @@
 export const LINKEDIN_LOGIN_URL = 'https://www.linkedin.com/login'
-export const LINKEDIN_API_URL = 'https://www.linkedin.com/voyager/api'
 
+export const LINKEDIN_REALTIME_URL = 'https://realtime.www.linkedin.com/realtime/connect'
+
+export const LINKEDIN_API_URL = 'https://www.linkedin.com/voyager/api'
 export const LINKEDIN_API_CONVERSATIONS_ENDPOINT = 'messaging/conversations'
 export const LINKEDIN_API_ME_ENDPOINT = 'me'
