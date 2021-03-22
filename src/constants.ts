847e6a063cd038ff1e915a2e711419838ae03df2
Emilio Villagran <emilio@villagran.cl>
2021-03-22 19:11:24 -0300

    feat(constants): add auth cookie constant and use it

--- src/constants.ts
+++ src/constants.ts
@@ -24,6 +24,8 @@ export const REQUEST_HEADERS: Record<string, string> = {
   'accept-language': 'en-US,en;q=0.9',
 }
 
+export const LinkedInAuthCookie = 'li_at'
+
 export const LinkedInURLs = {
   HOME: 'https://www.linkedin.com/',
   LOGIN: 'https://www.linkedin.com/login',
