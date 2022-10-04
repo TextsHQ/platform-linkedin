b45964f855bb1137b78f98a924851a9d7e94c4d0
Emilio Villagran <emilio@villagran.cl>
2022-10-04 15:54:31 -0300

    style: move encodeLinkedinUriComponent to util file

--- src/util.ts
+++ src/util.ts
@@ -29,3 +29,8 @@ export const getFeedUpdateURL = (feedUpdate: string) => {
   const baseUrl = 'https://www.linkedin.com/feed/update'
   return `${baseUrl}/${urn}`
 }
+
+export const encodeLinkedinUriComponent = (component: string): string => component.replace(
+  /[^A-Za-z0-9]/g,
+  (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`
+).replaceAll("%5F", "_").replaceAll('%2D', '-')
