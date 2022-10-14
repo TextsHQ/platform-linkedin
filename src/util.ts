9b9a4abe4895f3ab4aa0ee14904be1bda8cad92a
Emilio Villagran <emilio@villagran.cl>
2022-10-14 15:13:18 -0300

    feat(util): add extract first entity function

--- src/util.ts
+++ src/util.ts
@@ -3,9 +3,11 @@
 // urn:li:fsd_message:2-MTYxNzY2ODAyODc1N2IyNjY1MC0wMDQmZTI4OTlmNDEtOGI1MC00ZGEyLWI3ODUtNjM5NGVjYTlhNWIwXzAxMg==
 export const urnID = (entityUrn: string) => entityUrn.split(':').pop()
 
+export const extractFirstEntity = (urn: string): string => urn.split(':(', 2)?.[1].split(',')[0]
+
 // urn:li:fs_event:(2-ZTI4OTlmNDEtOGI1MC00ZGEyLWI3ODUtNjM5NGVjYTlhNWIwXzAxMg==,2-MTYxMzcwNjcxOTUzMWI2NTIzNi0wMDQmZTI4OTlmNDEtOGI1MC00ZGEyLWI3ODUtNjM5NGVjYTlhNWIwXzAxMg==)
 // urn:li:fs_updateV2:(urn:li:activity:6767570017279066112,MESSAGING_RESHARE,EMPTY,DEFAULT,false)
-export const eventUrnToThreadID = (eventUrn: string) => eventUrn.split(':(', 2)?.[1].split(',')[0]
+export const eventUrnToThreadID = extractFirstEntity
 
 export const eventUrnTupleToIDs = (eventUrn: string) => {
   const [, threadID, messageID] = /fs_event:\((.+),(.+)\)/.exec(eventUrn)
