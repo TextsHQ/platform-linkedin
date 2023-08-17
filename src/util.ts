ae802475a1c39011185b4ab36d374d3cc5f7961e
Emilio Villagran <emilio@villagran.cl>
2023-08-16 21:30:39 -0400

    feat(utils): add function to get tracking id

--- src/util.ts
+++ src/util.ts
@@ -55,3 +55,11 @@ export const debounce = (func: Function, wait: number) => {
     })
   }
 }
+
+export const getTrackingId = (): string => {
+  const randomIntArray = Array.from({ length: 16 }, () => Math.floor(Math.random() * 256))
+  const randByteArray = new Uint8Array(randomIntArray)
+  const charArray = Array.from(randByteArray, byte => String.fromCharCode(byte))
+
+  return charArray.join('')
+}
